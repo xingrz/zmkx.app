ed0723f250c6402c3ff17ab0c383a2bc92ec512c
XiNGRZ <hi@xingrz.me>
2023-01-17 03:08:35 +0800

    Introduce G2Plot

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@
       <li>目标电压: <code>{{ knobStatus.targetVoltage.toFixed(3) }}V</code></li>
     </ul>
   </a-space>
+  <div v-if="knobStatus" ref="lineEl" :style="{ height: '500px' }" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { Line } from '@antv/g2plot';
 
 import { query } from './comm';
 import { Action, MessageH2D, KnobStatus } from './proto/comm.proto';
+
+import usePlot from '@/composables/usePlot';
 
 const connected = ref(false);
 
@@ -60,11 +64,42 @@ setInterval(async () => {
     action: Action.KNOB_STATUS,
   }));
   knobStatus.value = res?.knobStatus;
+  if (res?.knobStatus) {
+    const s = res.knobStatus;
+    const t = [...timeline.value, {
+      ...s,
+      currentAngle: s.currentAngle % (Math.PI * 2),
+    }];
+    timeline.value = t.slice(t.length - 1000, t.length);
+  }
 }, 10);
 
 function radToAngle(rad: number): number {
   return 360 * (rad / Math.PI);
 }
+
+const timeline = ref<KnobStatus[]>([]);
+
+const { el: lineEl } = usePlot(timeline, (el, data) => new Line(el, {
+  autoFit: true,
+  animation: false,
+  data: data,
+  xField: 'timestamp',
+  yField: 'currentAngle',
+  tooltip: false,
+  xAxis: false,
+  yAxis: {
+    min: Math.PI * -2,
+    max: Math.PI * 2,
+  },
+  meta: {
+    currentAngle: {
+      min: Math.PI * -2,
+      max: Math.PI * 2,
+      formatter: (value: number) => radToAngle(value).toFixed(2),
+    },
+  },
+}));
 </script>
 
 <style lang="scss" module>
