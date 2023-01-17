ab593a3418e5d8c427ccce9ec63359c263b09f85
XiNGRZ <hi@xingrz.me>
2023-01-17 18:55:41 +0800

    Update completed Motor API

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <a-space direction="vertical" :style="{ margin: '100px' }">
     <a-button :disabled="connected" @click="connect">连接</a-button>
     <a-button :disabled="!connected" @click="queryVersion">版本</a-button>
-    <ul v-if="knobStatus" :class="$style.status">
-      <li>控制模式: <code>{{ knobStatus.controlMode }}</code></li>
-      <li>当前角度: <code>{{ radToAngle(knobStatus.currentAngle).toFixed(3) }}°</code></li>
-      <li>当前电压: <code>{{ knobStatus.currentVelocity.toFixed(3) }}V</code></li>
-      <li>目标角度: <code>{{ radToAngle(knobStatus.targetAngle).toFixed(3) }}°</code></li>
-      <li>目标速度: <code>{{ radToAngle(knobStatus.targetVelocity / 1000).toFixed(3) }}°/ms</code></li>
-      <li>目标电压: <code>{{ knobStatus.targetVoltage.toFixed(3) }}V</code></li>
+    <ul v-if="motorState" :class="$style.status">
+      <li>控制模式: <code>{{ motorState.controlMode }}</code></li>
+      <li>当前角度: <code>{{ radToAngle(motorState.currentAngle).toFixed(3) }}°</code></li>
+      <li>当前电压: <code>{{ motorState.currentVelocity.toFixed(3) }}V</code></li>
+      <li>目标角度: <code>{{ radToAngle(motorState.targetAngle).toFixed(3) }}°</code></li>
+      <li>目标速度: <code>{{ radToAngle(motorState.targetVelocity / 1000).toFixed(3) }}°/ms</code></li>
+      <li>目标电压: <code>{{ motorState.targetVoltage.toFixed(3) }}V</code></li>
     </ul>
   </a-space>
-  <div v-if="knobStatus" ref="lineEl" :style="{ height: '500px' }" />
+  <div v-if="motorState" ref="lineEl" :style="{ height: '500px' }" />
 </template>
 
 <script lang="ts" setup>
@@ -19,7 +19,7 @@ import { ref } from 'vue';
 import { Line } from '@antv/g2plot';
 
 import { query } from './comm';
-import { Action, MessageH2D, KnobStatus } from './proto/comm.proto';
+import { Action, MessageH2D, MotorState } from './proto/comm.proto';
 
 import usePlot from '@/composables/usePlot';
 
@@ -56,16 +56,16 @@ async function queryVersion() {
   console.log('output', res?.toJSON());
 }
 
-const knobStatus = ref<KnobStatus>();
+const motorState = ref<MotorState>();
 
 setInterval(async () => {
   if (!device) return;
   const res = await query(device, 1, MessageH2D.create({
-    action: Action.KNOB_STATUS,
+    action: Action.GET_MOTOR_STATE,
   }));
-  knobStatus.value = res?.knobStatus;
-  if (res?.knobStatus) {
-    const s = res.knobStatus;
+  motorState.value = res?.motorState;
+  if (res?.motorState) {
+    const s = res.motorState;
     const t = [...timeline.value, {
       ...s,
       currentAngle: s.currentAngle % (Math.PI * 2),
@@ -78,7 +78,7 @@ function radToAngle(rad: number): number {
   return 360 * (rad / Math.PI);
 }
 
-const timeline = ref<KnobStatus[]>([]);
+const timeline = ref<MotorState[]>([]);
 
 const { el: lineEl } = usePlot(timeline, (el, data) => new Line(el, {
   autoFit: true,
